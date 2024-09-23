7a64be92040b44b92ca6fd33504a48397042f399
eeeee333333ee33e <eacevedo@cpcecba.org.ar>
2024-09-22 21:08:28 -0300

    Agregado Jquery, fetch y manipulacion de DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,30 @@
-//inicio del programa.
 
-//defino constantes con los precios basicos:
+$(document).ready(function () {
+    $('#btnCalcular').click(function (event) {
+        event.preventDefault();
+        iniciaCalculadora()
+    });
+});
 
-const mDerProf = 7000;
-const mDerProfRd = mDerProf / 2;
-const mSsociales = 15000;
-const mSsocialesFam = 17000;
-const mCps = 10000;
-const mTxtSsOblig = "(Afiliación obligatoria por matricula plena) ";
-const valores = {dep:mDerProf, depRd:mDerProfRd, ssociales: mSsociales, ssocialesFam : 17000, cps: 10000 };
+$(document).ready(function () {
+    $('#btnContinuar').click(function (event) {
+        event.preventDefault();
+        finalizoCalculadora()
+    });
+});
 
-function buscoValorObjetoSimpleNumerico(objeto, clave){
+fetch('https://cpcecba.org.ar/servicios/val.json')
+    .then(response => {
+        
+        return response.json();
+    })
+    .then(data => {
+        valores.push(...data);
+        presentoMontos(valores);
+    })
+
+
+function buscoValorObjetoSimpleNumerico(objeto, clave) {
     return parseInt(objeto[clave]);
 }
 
@@ -18,86 +32,118 @@ function iniciaCalculadora() {
     var iNom = undefined;
     var iTip = undefined;
     var iSsocOpc = undefined;
+    let mAportaCps = false;
+    let lblCps = $("#lblCps");
     var mTot = 0;
     //ahora, sumamos en un objeto.
     var mResumen = [];
-    while (chkNull(iNom)) {
-        iNom = prompt("Bienvenido al Consejo, por favor, ingrese su nombre para asesorarlo mejor.")
-    }
 
-    while ((iTip != "P") && (iTip != "RD")) {
-        iTip = prompt("Muchas gracias " + iNom + ", por favor, ahora ingrese las letras P si desea una matricula plena, o RD si desea una matricula en relación de dependencia. Debe escribirlas en mayusculas.")
-    }
+    iTip = $("#inputTipoMat").val();
+
     let mVal = 0;
     switch (iTip) {
-        
+
         case "P":
             //sumo derecho prof pleno
             mVal = buscoValorObjetoSimpleNumerico(valores, "dep");
-            mResumen.push({"Derecho por Ejercicio Profesional": mVal});
+            mResumen.push({ "Derecho por Ejercicio Profesional": mVal });
             mVal = 0;
             //sumo la obra social obligatoria
-            mVal = buscoValorObjetoSimpleNumerico(valores, "ssociales");
-            mResumen.push({"Cuota Servicios Sociales": mVal});
+
+
             mVal = 0;
             //vamos a la rutina que nos calcula servicios sociales... para los familiares..
-            mVal = calculadoraServiciosSociales(true);
-            if (mVal > 0) {
-                mResumen.push({"Complemento S. Sociales - Familiares": mVal});    
-            }
-            mVal = 0
-            //finalmente, sumamos la caja, que tambien es obligatoria
+            inicioCalculadoraServiciosSociales(true);
+
+            mAportaCps = true;
             mVal = buscoValorObjetoSimpleNumerico(valores, "cps");
-            mResumen.push({"Aportes Caja de Prev. Social PCEC": mVal});    
-            alert("Se sumaron $" + buscoValorObjetoSimpleNumerico(valores, "cps") + ", en concepto de la CPS, que es obligatoria por ser plena la matricula.")
-            break;
+            mResumen.push({ "Aportes Caja de Prev. Social PCEC": mVal });
+            lblCps.text("Se sumaron conceptos en aporte a la Caja de Jubilaciones - obligatorio");
+            lblCps.show();
+            guardoLs(mResumen);
 
         case "RD":
+            //sumo derecho prof pleno
             mVal = buscoValorObjetoSimpleNumerico(valores, "depRd");
-            mResumen.push({"Derecho por Ej. Prof. - Rel. de Dependencia": mVal});
+            mResumen.push({ "Derecho por Ejercicio Profesional - Relacion de Dependencia": mVal });
             mVal = 0;
+            //sumo la obra social obligatoria
 
-            iSsocOpc = prompt("Sr. Profesional, lo invitamos a afiliarse a Servicios Sociales, escriba NO para no afiliarse. Debe estar en mayúsculas, caso contrario lo afiliaremos!.");
 
-            if (iSsocOpc != "NO") {
-                mVal = buscoValorObjetoSimpleNumerico(valores, "ssociales");
-                mResumen.push({"Cuota Servicios Sociales": mVal});
-                mVal = 0;
-                mVal = calculadoraServiciosSociales(false);
-                if (mVal > 0) {
-                    mResumen.push({"Complemento S. Sociales - Familiares": mVal});
-                }
-                
-            }
-            alert("Ud. Esta exento de la CPS por ser matriculado en Relación de Dependencia. Disfrute el ahorro, y no tener jubilación (igual no le ibamos a pagar tanto).");
+            mVal = 0;
+            //vamos a la rutina que nos calcula servicios sociales... para los familiares..
+            inicioCalculadoraServiciosSociales(false);
+
+            mAportaCps = false;
+        
+            lblCps.text("No se sumaron conceptos en aporte a la Caja de Jubilaciones ya que es afiliado en relacion de dependencia, de todas formas no le ibamos a pagar tanto...");
+            lblCps.show();
+            guardoLs(mResumen);
 
             break;
+
+
 
         default:
-            iniciaCalculadora();
+
             break;
-            
+
     }
-    presentoMontos(iNom, mResumen);
+
 }
 
-
-function calculadoraServiciosSociales(mSsOblig = false) {
-    var txtExtra = "";
-    var iFamiliares = undefined;
-    if (mSsOblig) {
-        txtExtra = mTxtSsOblig;
-    }
-    //Esta funcion devuelve el monto de servicios sociales
-    //aca no uso chknull por que pueden responder 0...
-
-    while (true) {
-        iFamiliares = parseInt(prompt("Su monto de Servicios Sociales " + txtExtra + "es de: $ " + buscoValorObjetoSimpleNumerico(valores, "ssociales") + ", si desea sumar familiares opcionales, escriba la cantidad, caso contrario, escriba el numero 0 (cero)."));
-        if (!isNaN(iFamiliares) && iFamiliares >= 0) {
-            break;
+function finalizoCalculadora() {
+    let mResumen = buscoLs("CalculadoraW7");
+    
+    cmbSsoc = $("#inputAfilSSoc");
+    qAfilSsoc = parseInt($("#inputSsocOpt").val());
+    //Calculo los montos de SSOC nomas y ya estariamos...
+    if (cmbSsoc.val() == "Y") {
+        mVal = buscoValorObjetoSimpleNumerico(valores, "ssociales");
+        mResumen.push({ "Cuota de Afiliacion Servicios Sociales": mVal });
+        mVal = 0;
+        if (qAfilSsoc > 0) {
+            mVal = (buscoValorObjetoSimpleNumerico(valores, "ssocialesFam") * qAfilSsoc);
+            mResumen.push({ "Complemento Prestacion Ssociales Familiares": mVal });
+            mVal = 0;
         }
     }
-    return parseInt(montoOptSsociales(iFamiliares));
+    guardoLs(mResumen);
+    $("#cntResultado").show();
+    presentoMontos(mResumen);
+}
+
+function guardoLs(dato) {
+    let mDatGuardar = JSON.stringify(dato)
+    localStorage.setItem("CalculadoraW7", mDatGuardar);
+}
+
+function buscoLs(clave) {
+    let mData = JSON.parse(localStorage.getItem(clave));
+    return mData;
+}
+
+function inicioCalculadoraServiciosSociales(mSsOblig = false) {
+    let txtExtra = "";
+    let iFamiliares = undefined;
+    let lbl = $("#lblAfiliacionSsoc");
+    let selectorSsoc = $("#inputAfilSSoc");
+    let cntSsoc = $("#cntSsoc");
+    if (mSsOblig) {
+        txtExtra = mTxtSsOblig;
+        lbl.text("Servicios sociales:" + mTxtSsOblig);
+        selectorSsoc.empty();
+        selectorSsoc.append(`
+            <option value="Y">Si</option>
+          `);
+    } else {
+        selectorSsoc.empty();
+        selectorSsoc.append(`
+            <option value="Y">Si</option>
+            <option value="N">No</option>
+          `);
+    }
+    cntSsoc.show();
 }
 
 function montoOptSsociales(qFamiliares = 0) {
@@ -120,17 +166,30 @@ function chkNull(value) {
     return false;
 }
 
-function presentoMontos(iNom, mResumen) {
-    var total = 0;
-    var txtAlert = "Estimado profesional - "+iNom+": le acercamos como se conformará su cedulón de pago:\n";
-    mResumen.forEach(item => {
-        Object.entries(item).forEach(([clave, valor]) => {
-            txtAlert += "$ " + valor + " - " + clave + ".- \n";
-            total = total + valor;
-        });
+function presentoMontos(resumen) {
+    $('#grdResultado tbody').empty();
+
+    let total = 0;
+
+    resumen.forEach(item => {
+        const itemName = Object.keys(item)[0];
+        const amount = item[itemName];
+        total += amount;
+
+        $('#grdResultado tbody').append(`
+            <tr>
+                <td>${itemName}</td>
+                <td>${amount}</td>
+            </tr>
+        `);
     });
-    txtAlert += "==================\n";
-    txtAlert += "Total: $ " + total + ".- Gracias por usar nuestra calculadora";
-    alert(txtAlert);
-    return;
+
+    $('#grdResultado tbody').append(`
+        <tr>
+            <td><strong>Total</strong></td>
+            <td><strong>${total}</strong></td>
+        </tr>
+    `);
+
+    $('#cntResultado').show();
 }
